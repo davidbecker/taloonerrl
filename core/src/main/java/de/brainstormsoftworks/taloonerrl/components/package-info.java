cd1bffd5167358072d48d3264e8905b538f8b49f
dasmau89 <dasmau89@googlemail.com>
2015-08-01 19:15:07 +0200

    added missing copyright info & package-info

---
+++ core/src/main/java/de/brainstormsoftworks/taloonerrl/components/package-info.java
@@ -0,0 +1,4 @@
+/*
+ * this package contains components for entities
+ */
+package de.brainstormsoftworks.taloonerrl.components;
---
+++ core/src/main/java/de/brainstormsoftworks/taloonerrl/system/package-info.java
@@ -0,0 +1,4 @@
+/*
+ * this package contains systems for the entity system
+ */
+package de.brainstormsoftworks.taloonerrl.system;
