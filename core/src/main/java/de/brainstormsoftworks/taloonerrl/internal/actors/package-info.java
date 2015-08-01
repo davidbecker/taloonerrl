88863f164ab3b90ed2ff548bd71c4571ddfcb598
dasmau89 <dasmau89@googlemail.com>
2015-08-01 13:20:20 +0200

    added package info files

---
+++ core/src/main/java/de/brainstormsoftworks/taloonerrl/internal/actors/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * this package contains the implementation details for the actors. classes in
+ * this package shouldn't be referenced by classes that aren't in
+ * de.brainstormsoftworks.taloonerrl.actors
+ */
+package de.brainstormsoftworks.taloonerrl.internal.actors;
