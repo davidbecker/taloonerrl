3ae670be5f98ca2f96609b5bf78be3d7f4a543c6
dasmau89 <dasmau89@googlemail.com>
2015-08-01 15:11:56 +0200

    commented out code

--- core/src/main/java/de/brainstormsoftworks/taloonerrl/system/MovementSystem.java
+++ core/src/main/java/de/brainstormsoftworks/taloonerrl/system/MovementSystem.java
@@ -1,13 +1,11 @@
 package de.brainstormsoftworks.taloonerrl.system;
 
 import com.artemis.Aspect;
-import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.systems.EntityProcessingSystem;
 
 import de.brainstormsoftworks.taloonerrl.components.ControllerComponent;
 import de.brainstormsoftworks.taloonerrl.components.PositionComponent;
-import de.brainstormsoftworks.taloonerrl.core.engine.ComponentMappers;
 
 public class MovementSystem extends EntityProcessingSystem {
 
@@ -18,16 +16,26 @@ public class MovementSystem extends EntityProcessingSystem {
 
 	@Override
 	protected void process(final Entity entity) {
-		// TODO implement collision
-		final ComponentMapper<PositionComponent> posMapper = ComponentMappers.getInstance().position;
-		final ComponentMapper<ControllerComponent> controllerMapper = ComponentMappers.getInstance().controller;
-		final PositionComponent position = posMapper.get(entity);
-		final ControllerComponent controllerComponent = controllerMapper.get(entity);
-		// adds the vector to the position
-		position.setX(position.getX() + controllerComponent.getMoveVector().getX());
-		position.setY(position.getY() + controllerComponent.getMoveVector().getY());
-		// resets controller vector
-		controllerComponent.getMoveVector().setZero();
+		/*
+		 * FIXME probably shouldn't be done this way - implement event system
+		 * for turn based entities instead I'm considering this approach for
+		 * particles (if needed)
+		 */
+		// // TODO implement collision
+		// final ComponentMapper<PositionComponent> posMapper =
+		// ComponentMappers.getInstance().position;
+		// final ComponentMapper<ControllerComponent> controllerMapper =
+		// ComponentMappers.getInstance().controller;
+		// final PositionComponent position = posMapper.get(entity);
+		// final ControllerComponent controllerComponent =
+		// controllerMapper.get(entity);
+		// // adds the vector to the position
+		// position.setX(position.getX() +
+		// controllerComponent.getMoveVector().getX());
+		// position.setY(position.getY() +
+		// controllerComponent.getMoveVector().getY());
+		// // resets controller vector
+		// controllerComponent.getMoveVector().setZero();
 	}
 
 }
