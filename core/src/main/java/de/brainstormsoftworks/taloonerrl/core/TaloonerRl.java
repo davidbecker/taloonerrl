f7ba71c744225644de26e34b2322e908c45f4fb3
dasmau89 <dasmau89@googlemail.com>
2015-08-02 06:06:03 +0200

    moved more rendering functionality out of main class

--- core/src/main/java/de/brainstormsoftworks/taloonerrl/core/TaloonerRl.java
+++ core/src/main/java/de/brainstormsoftworks/taloonerrl/core/TaloonerRl.java
@@ -14,13 +14,9 @@ import com.artemis.Entity;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 import de.brainstormsoftworks.taloonerrl.actors.ActorFactory;
@@ -65,17 +61,9 @@ public class TaloonerRl implements ApplicationListener {
 	Animation walkLeft;
 	Animation walkRight;
 	EDirection walkingDirection = EDirection.RIGHT;
-	private static final int tileSize = 16;
 	private static final float scale = 16f;
 	float mouseX;
 	float mouseY;
-	private static final int TILES_HORIZONTAL = 30;
-	private static final int TILES_VERTICAL = 20;
-	private static final int VIRTUAL_WIDTH = TILES_HORIZONTAL * tileSize + 4 * tileSize;
-	private static final int VIRTUAL_HEIGHT = TILES_VERTICAL * tileSize;
-	private static final float ASPECT_RATIO = (float) VIRTUAL_WIDTH / (float) VIRTUAL_HEIGHT;
-	private OrthographicCamera camera;
-	private Rectangle viewport;
 
 	private boolean isPlayerTurn = false;
 	private float delayToNextTurn = 0f;
@@ -94,14 +82,18 @@ public class TaloonerRl implements ApplicationListener {
 
 	@Override
 	public void create() {
-		map = MapFactory.createMap(TILES_HORIZONTAL, TILES_VERTICAL);
+		map = MapFactory.createMap(Renderer.TILES_HORIZONTAL, Renderer.TILES_VERTICAL);
 
 		warrior = new Texture(Gdx.files.internal("character/Warrior.png"));
 		for (int i = 0; i < 4; i++) {
-			walkFramesDown[i] = new TextureRegion(warrior, i * tileSize, 0 * tileSize, tileSize, tileSize);
-			walkFramesLeft[i] = new TextureRegion(warrior, i * tileSize, 1 * tileSize, tileSize, tileSize);
-			walkFramesRight[i] = new TextureRegion(warrior, i * tileSize, 2 * tileSize, tileSize, tileSize);
-			walkFramesUp[i] = new TextureRegion(warrior, i * tileSize, 3 * tileSize, tileSize, tileSize);
+			walkFramesDown[i] = new TextureRegion(warrior, i * Renderer.tileSize, 0 * Renderer.tileSize,
+					Renderer.tileSize, Renderer.tileSize);
+			walkFramesLeft[i] = new TextureRegion(warrior, i * Renderer.tileSize, 1 * Renderer.tileSize,
+					Renderer.tileSize, Renderer.tileSize);
+			walkFramesRight[i] = new TextureRegion(warrior, i * Renderer.tileSize, 2 * Renderer.tileSize,
+					Renderer.tileSize, Renderer.tileSize);
+			walkFramesUp[i] = new TextureRegion(warrior, i * Renderer.tileSize, 3 * Renderer.tileSize,
+					Renderer.tileSize, Renderer.tileSize);
 		}
 		walkUp = new Animation(0.25f, walkFramesUp);
 		walkDown = new Animation(0.25f, walkFramesDown);
@@ -116,8 +108,6 @@ public class TaloonerRl implements ApplicationListener {
 
 		mouseX = Gdx.graphics.getWidth() / 2;
 		mouseY = Gdx.graphics.getHeight() / 2;
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
 
 		player.getMovementComponent().move(4, 4);
 		// forces the engine to initialize
@@ -132,8 +122,8 @@ public class TaloonerRl implements ApplicationListener {
 		createNewEntity.getComponent(HealthComponent.class).setHealthPercent(0.75f);
 
 		gameEngine.createNewEntity(EEntity.BLOB, 2, 2);
-		for (int i = 1; i < TILES_HORIZONTAL - 1; i++) {
-			gameEngine.createNewEntity(EEntity.TORCH, i, TILES_VERTICAL - 1);
+		for (int i = 1; i < Renderer.TILES_HORIZONTAL - 1; i++) {
+			gameEngine.createNewEntity(EEntity.TORCH, i, Renderer.TILES_VERTICAL - 1);
 		}
 
 		DungeonRenderer.initInstance();
@@ -142,23 +132,7 @@ public class TaloonerRl implements ApplicationListener {
 
 	@Override
 	public void resize(final int width, final int height) {
-		// calculate new viewport
-		final float aspectRatio = (float) width / (float) height;
-		float scale = 1f;
-		final Vector2 crop = new Vector2(0f, 0f);
-		if (aspectRatio > ASPECT_RATIO) {
-			scale = (float) height / (float) VIRTUAL_HEIGHT;
-			crop.x = (width - VIRTUAL_WIDTH * scale) / 2f;
-		} else if (aspectRatio < ASPECT_RATIO) {
-			scale = (float) width / (float) VIRTUAL_WIDTH;
-			crop.y = (height - VIRTUAL_HEIGHT * scale) / 2f;
-		} else {
-			scale = (float) width / (float) VIRTUAL_WIDTH;
-		}
-
-		final float w = VIRTUAL_WIDTH * scale;
-		final float h = VIRTUAL_HEIGHT * scale;
-		viewport = new Rectangle(crop.x, crop.y, w, h);
+		Renderer.getInstance().resizeViewPort(width, height);
 	}
 
 	@Override
@@ -239,26 +213,13 @@ public class TaloonerRl implements ApplicationListener {
 			playerHealthComponent.setHealthPercent(1.0f);
 		}
 
-		camera.update();
-
-		// see http://codebin.co.uk/blog/pixelated-rendering-in-libgdx/
-
-		final int viewPortX = Math.round(viewport.x * tileSize) / tileSize;
-		final int viewPortY = Math.round(viewport.y * tileSize) / tileSize;
-		final int viewPortWidth = Math.round(viewport.width * tileSize) / tileSize;
-		final int viewPortHeight = Math.round(viewport.height * tileSize) / tileSize;
-		Gdx.gl.glViewport(viewPortX, viewPortY, viewPortWidth, viewPortHeight);
-		Gdx.graphics.setTitle("current fps: " + Gdx.graphics.getFramesPerSecond());
 		elapsed += deltaTime;
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Renderer.getInstance().BATCH.setProjectionMatrix(camera.combined);
-		Renderer.getInstance().BATCH.begin();
-		Renderer.getInstance().BATCH.enableBlending();
+		Renderer.getInstance().beginRendering();
 
-		DungeonRenderer.getInstance().render(Renderer.getInstance().BATCH, map.getMap(), TILES_HORIZONTAL,
-				TILES_VERTICAL);
-		GuiRenderer.getInstance().render(Renderer.getInstance().BATCH, TILES_HORIZONTAL, TILES_VERTICAL);
+		DungeonRenderer.getInstance().render(Renderer.getInstance().BATCH, map.getMap(), Renderer.TILES_HORIZONTAL,
+				Renderer.TILES_VERTICAL);
+		GuiRenderer.getInstance().render(Renderer.getInstance().BATCH, Renderer.TILES_HORIZONTAL,
+				Renderer.TILES_VERTICAL);
 
 		TextureRegion currentFrame = null;
 		switch (walkingDirection) {
@@ -283,21 +244,21 @@ public class TaloonerRl implements ApplicationListener {
 
 		final Vector3 touchPos = new Vector3();
 		touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-		camera.unproject(touchPos, viewPortX, viewPortY, viewPortWidth, viewPortHeight);
-		mouseX = touchPos.x - tileSize / 2;
-		mouseY = touchPos.y - tileSize / 2;
+		Renderer.getInstance().unprojectFromCamera(touchPos);
+		mouseX = touchPos.x - Renderer.tileSize / 2;
+		mouseY = touchPos.y - Renderer.tileSize / 2;
 
 		Renderer.getInstance().BATCH.draw(cursor, mouseX, mouseY);
 		// FIXME WIP
 		// // translate mouse coordinates to selected tile
-		// final float tileX = touchPos.x / TILES_HORIZONTAL; // 0-18
-		// final float tileY = touchPos.y / TILES_VERTICAL;
+		// final float tileX = touchPos.x /Renderer.TILES_HORIZONTAL; // 0-18
+		// final float tileY = touchPos.y / Renderer.TILES_VERTICAL;
 		// Renderer.getInstance().BATCH.draw(cursor, tileX, tileY);
 		// System.out.println(tileX + " " + tileY);
 		highlightPlayerTile(player);
 
 		GameEngine.getInstance().update(deltaTime);
-		Renderer.getInstance().BATCH.end();
+		Renderer.getInstance().endRendering();
 
 		isPlayerTurn = false;
 	}
