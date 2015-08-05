50e9a8ca4aa7e28260d4a5ce8bf3ff31f3f18b96
dasmau89 <dasmau89@googlemail.com>
2015-08-05 15:45:35 +0200

    removed magic numbers

--- desktop/src/main/java/de/brainstormsoftworks/taloonerrl/java/TaloonerRlDesktop.java
+++ desktop/src/main/java/de/brainstormsoftworks/taloonerrl/java/TaloonerRlDesktop.java
@@ -4,12 +4,25 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import de.brainstormsoftworks.taloonerrl.core.TaloonerRl;
+import de.brainstormsoftworks.taloonerrl.render.Renderer;
 
+/**
+ * wrapper class to start a new desktop game
+ *
+ * @author David Becker
+ *
+ */
 public class TaloonerRlDesktop {
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.height = 20 * 16;
-		config.width = 30 * 16;
+
+	/**
+	 * starts a new game
+	 *
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = Renderer.VIRTUAL_HEIGHT;
+		config.width = Renderer.VIRTUAL_WIDTH;
 		new LwjglApplication(new TaloonerRl(), config);
 	}
 }
